599f5972047e59da42a4b47c8c4da21cf493de56
raghav <aroraraghav397@gmail.com>
2021-05-05 18:59:04 +0530

    message

--- js/Game.js
+++ js/Game.js
@@ -33,6 +33,10 @@ class Game {
     car2 = createSprite(300,200);
     car3 = createSprite(500,200);
     car4 = createSprite(700,200);
+    car1.addImage(car1Img)
+    car2.addImage(car2Img)
+    car3.addImage(car3Img)
+    car4.addImage(car4Img)
     cars = [car1, car2, car3, car4];
   }
 
@@ -43,14 +47,15 @@ class Game {
     
     if(allPlayers !== undefined){
       //var display_position = 100;
-      
+      background(groundImg)
+      image(trackImg,0,-displayHeight*4,displayWidth,displayHeight*5 )
       //index of the array
       var index = 0;
 
       //x and y position of the cars
-      var x = 0;
+      var x = 175;
       var y;
-
+ 
       for(var plr in allPlayers){
         //add 1 to the index for every loop
         index = index + 1 ;
@@ -76,9 +81,15 @@ class Game {
 
     if(keyIsDown(UP_ARROW) && player.index !== null){
       player.distance +=10
+      console.log(player.distance)
       player.update();
     }
-
+   if (player.distance>3430){
+     gameState=2
+   }
     drawSprites();
+  }
+  end(){
+    console.log("Game Ended" )
   }
 }
--- sketch.js
+++ sketch.js
@@ -10,7 +10,14 @@ var form, player, game;
 
 var cars, car1, car2, car3, car4;
 
-
+function preload(){
+  car1Img=loadImage("images/car1.png")
+  car2Img=loadImage("images/car2.png")
+  car3Img=loadImage("images/car3.png")
+  car4Img=loadImage("images/car4.png")
+  groundImg=loadImage("images/ground.png")
+  trackImg=loadImage("images/track.jpg")
+}
 function setup(){
   canvas = createCanvas(displayWidth - 20, displayHeight-30);
   database = firebase.database();
@@ -27,5 +34,8 @@ function draw(){
   if(gameState === 1){
     clear();
     game.play();
+  }
+  if (gameState===2){
+    game.end()
   }
 }
